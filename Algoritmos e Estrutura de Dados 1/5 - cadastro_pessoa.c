f05abe7a5bfe0932b019f7fc258fed778e2739d3
PabloSustisso <pablogabrielsustisso0@gmail.com>
2023-09-05 21:43:18 -0300

    scanf's na função de cadastro

--- Algoritmos e Estrutura de Dados 1/5 - cadastro_pessoa.c
+++ Algoritmos e Estrutura de Dados 1/5 - cadastro_pessoa.c
@@ -30,16 +30,51 @@ typedef struct
     char nome[100];
     rg_cnh rg_cnh;
     char sexo;
-    char tipo_sangue[2];
+    char tipo_sangue[3];
     char cpf[11];
     data data_nasc;
     endereco endereco;
 }Pessoa;
 
-void criar_lista_contatos(int n, Pessoa pessoas[n])
+void preencher_lista_contatos(int n, Pessoa pessoas[n])
 {
+    char opc_doc;
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Nome (ate 100 caracteres): ");
+        scanf("%s", pessoas[i].nome[100]);
+
+        printf("(RG - R) ou (CNH - C)?");
+        scanf("%c", &opc_doc);
+        if (opc_doc == 'R' || 'r')
+        {
+            printf("RG: ");
+            scanf("%s", pessoas[i].rg_cnh.rg[9]);
+        }
+        else if (opc_doc == 'C' || 'c')
+        {
+            printf("CNH: ");
+            scanf("%s", pessoas[i].rg_cnh.cnh[11]);
+        }
+        
+        printf("Sexo (M/F):");
+        scanf("%c", pessoas[i].sexo);
+
+        printf("Tipo sanguineo (A+, AB-, ...): ");
+        scanf("%s", pessoas[i].tipo_sangue[3]);
+        while ((pessoas[i].tipo_sangue) != "A+" || "A-" || "B+" || "B-" || "AB+" || "AB-" || "O+" || "O-")
+        {
+            printf("\nPor favor, insira um tipo sanguineo valido!\n");
+            printf("Tipo sanguineo (A+, AB-, ...): ");
+            scanf("%s", pessoas[i].tipo_sangue[3]);
+        }
     
-    
+        printf("CPF: ");
+        scanf("%s", pessoas[i].cpf[11]);
+
+        printf("Data de nascimento: ");
+    }
 }
 
 int main(void)
