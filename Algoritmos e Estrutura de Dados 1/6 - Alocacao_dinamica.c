294dcdfe77f3ab921a9d41afd3379310f0683981
PabloSustisso <pablogabrielsustisso0@gmail.com>
2023-09-12 19:31:07 -0300

    Alocação Dinâmica e correção no método da bisseção

---
+++ Algoritmos e Estrutura de Dados 1/6 - Alocacao_dinamica.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+int main(void)
+{
+    char *s;
+    // armazenar 12 caracteres e \0
+    s = (char *)malloc(13 * sizeof(char));
+    // colocando uma frase na string
+    strcpy(s, "kame hame ha");
+    // realocar espaço para mais um caractere
+    s = (char *)realloc(s, 14 * sizeof(char));
+    // adicionando o ponto de exclamação à string
+    strcat(s, "!");
+
+    s = (char *)realloc(s, 15 * sizeof(char));
+    strcat(s, "p");
+
+    printf("%s", s);
+
+    return 0;
+}
--- Cálculo Numérico/metodo_da_bissecao.c
+++ Cálculo Numérico/metodo_da_bissecao.c
@@ -9,44 +9,60 @@ int main(void)
 {
     float precisao;
     float a, b, Xm, f_a, f_b, f_xm, erro;
+    int n_iteracoes;
 
     printf("Digite o intervalo do zero da funcao: ");
     scanf("%f %f", &a, &b);
     printf("Digite a precisao desejada: ");
     scanf("%f", &precisao);
 
-    Xm = (a+b)/2;
-    f_a = sin(a);
-    f_b = 4 * sin(b);
-    f_xm = 4 * sin(Xm);
-    erro = b-a;
+    // Xm = (a + b) / 2;
+    // f_a = 70 * (pow(e, (-1.5 * a))) + 2.5 * (pow(e, (-0.075 * a))) - 9;
+    // f_b = 70 * (pow(e, (-1.5 * b))) + 2.5 * (pow(e, (-0.075 * b))) - 9;
+    // f_xm = 70 * (pow(e, (-1.5 * Xm))) + 2.5 * (pow(e, (-0.075 * Xm))) - 9;
+    // erro = b - a;
+
+    Xm = (a + b) / 2;
+    f_a = a * log10(a) - 1;
+    f_b = b * log10(b) - 1;
+    f_xm = Xm * log10(Xm) - 1;
+    erro = (b - a);
+
+    n_iteracoes = ceil((log10(b - a) - log10(precisao)) / (log10(2)));
+    printf("\nNumero de iteracoes: %d\n", n_iteracoes);
 
     printf("\na\tXm\tb\tf(a)\tf(Xm)\tf(b)\terro\n");
     printf("%.2f\t%.4f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n", a, Xm, b, f_a, f_xm, f_b, erro);
-    
-    while ((b-a) > precisao)
+
+    while (erro > precisao)
     {
-        if ((b-a) <= precisao)
+        if (erro <= precisao)
         {
             break;
         }
-        if ((f_a*f_xm) < 0)
+        if ((f_a * f_xm) < 0)
         {
             b = Xm;
         }
-        else if ((f_xm*f_b) < 0)
+        else if ((f_xm * f_b) < 0)
         {
             a = Xm;
         }
-        
-        Xm = (a+b)/2;
-        f_a = sin(a);
-        f_b = 4 * sin(b);
-        f_xm = 4 * sin(Xm);
-        erro = erro/2;
+
+        // Xm = (a + b) / 2;
+        // f_a = 70 * (pow(e, (-1.5 * a))) + 2.5 * (pow(e, (-0.075 * a))) - 9;
+        // f_b = 70 * (pow(e, (-1.5 * b))) + 2.5 * (pow(e, (-0.075 * b))) - 9;
+        // f_xm = 70 * (pow(e, (-1.5 * Xm))) + 2.5 * (pow(e, (-0.075 * Xm))) - 9;
+        // erro = b - a;
+
+        Xm = (a + b) / 2;
+        f_a = a * log10(a) - 1;
+        f_b = b * log10(b) - 1;
+        f_xm = Xm * log10(Xm) - 1;
+        erro = (b - a);
 
         printf("%.2f\t%.4f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n", a, Xm, b, f_a, f_xm, f_b, erro);
     }
-    
+
     return 0;
 }
