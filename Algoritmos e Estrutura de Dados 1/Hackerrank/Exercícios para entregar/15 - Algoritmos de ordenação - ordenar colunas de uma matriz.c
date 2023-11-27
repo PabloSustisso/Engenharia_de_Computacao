6dec226a75e3faf2273da44a172f691892edf54e
PabloSustisso <pablogabrielsustisso0@gmail.com>
2023-11-27 13:14:26 -0300

    challenge finished

--- Algoritmos e Estrutura de Dados 1/Hackerrank/Exercícios para entregar/15 - Algoritmos de ordenação - ordenar colunas de uma matriz.c
+++ Algoritmos e Estrutura de Dados 1/Hackerrank/Exercícios para entregar/15 - Algoritmos de ordenação - ordenar colunas de uma matriz.c
@@ -1,25 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *iniciar(int l, int c)
+float **startMat(int l, int c)
 {
-    int **mat;
-    mat = (int**)malloc(l * sizeof(int*));
+    float **mat;
+    mat = (float**)malloc(l * sizeof(float*));
     
     for (int i = 0; i < l; i++) // Alocando dinamicamente as colunas da matriz
     {
-        mat[i] = (int*)malloc(c * sizeof(int));
+        mat[i] = (float*)malloc(c * sizeof(float));
     }
 
     return mat;
 }
 
+void scanMat(float **mat, int l, int c)
+{
+    for (int i = 0; i < l; i++)
+        for (int j = 0; j < c; j++)
+            scanf("%f", &mat[i][j]);
+}
+
+void printMat(float **mat, int l, int c)
+{
+    for (int i = 0; i < l; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            printf("%.1f ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void sortColumnMat(float **mat, int l, int c)
+{
+    float *v;
+    int k = 0, i, j, x;
+
+    v = (float*)malloc(l * sizeof(float));
+    
+    while (k < c)
+    {
+        // atribuir cada coluna separadamente a um vetor auxiliar
+        for (i = 0; i < l; i++)
+            v[i] = mat[i][k];
+        
+        // ordenar separadamente cada coluna utilizando bubblesort
+        for (i = 0; i < l - 1; i++)
+            for (j = 0; j < l - i - 1; j++)
+                if (v[j] > v[j + 1]){
+                    x = v[j];
+                    v[j] = v[j + 1];
+                    v[j + 1] = x;
+                }
+
+        // atribuir os valores ordenados para matriz em ordem decrescente
+        int z = 0;
+        for (i = l - 1; i >= 0; i--){
+            mat[z][k] = v[i];
+            z++;
+        }
+
+        k++;
+    } 
+    
+}
+
 int main(void)
 {
-    int **mat, l, c;
+    int l, c;
+    float **mat;
     scanf("%d %d", &l, &c);
 
-    mat = iniciar(l, c);
+    mat = startMat(l, c);
+
+    scanMat(mat, l, c);
+    sortColumnMat(mat, l , c);
+    printMat(mat, l , c); // matriz ja ordenada
 
     return 0;
 }
