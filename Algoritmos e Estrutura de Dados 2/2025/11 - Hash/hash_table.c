5317f59873a2c736dfc65bf8a83082d24d206c9e
PabloSustisso <pablogabrielsustisso0@gmail.com>
2025-06-24 18:26:01 -0300

    hashtable

--- Algoritmos e Estrutura de Dados 2/2025/11 - Hash/hash_table.c
+++ Algoritmos e Estrutura de Dados 2/2025/11 - Hash/hash_table.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct{
+    int tamanho;
+    int *buckets;
+}HashTable;
+
+HashTable* gerarHT(unsigned int tam){
+
+}
+
+int buscar(unsigned int chave, HashTable *t){
+
+}
+
+int inserir(unsigned int , HashTable *t){
+
+}
+
+int remover(unsigned int key)
